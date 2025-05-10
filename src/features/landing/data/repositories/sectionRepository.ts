526ebceb5aa44db3fc2f9a51153abf642e6835ec
thanhbach07 <bachttps41294@gmail.com>
2025-05-10 09:14:55 +0700

    feat(feature): invoice details

--- src/features/landing/data/repositories/sectionRepository.ts
+++ src/features/landing/data/repositories/sectionRepository.ts
@@ -1,29 +1,35 @@
-import { SectionType } from '@prisma/client';
-import { decorate, injectable } from 'inversify';
-import { ISection } from '../../domain/interfaces/Section';
-import type { ILandingAPI } from '../api/api';
+// import { SectionType } from '@prisma/client';
+// import { decorate, injectable } from 'inversify';
+// import { ISection } from '../../domain/interfaces/Section';
+// import type { ILandingAPI } from '../api/api';
 
-export interface ISectionRepository {
-  getSection(sectionType: SectionType): Promise<ISection>;
-}
+// export interface ISectionRepository {
+//   getSection(sectionType: SectionType): Promise<ISection>;
+// }
 
-export class SectionRepository implements ISectionRepository {
-  private landingAPI: ILandingAPI;
+// export class SectionRepository implements ISectionRepository {
+//   static getSectionsByLessonId(_arg0: string): any[] | PromiseLike<any[]> {
+//       throw new Error('Method not implemented.');
+//   }
+//   static getSectionsByCourseId(_arg0: string): any[] | PromiseLike<any[]> {
+//       throw new Error('Method not implemented.');
+//   }
+//   private landingAPI: ILandingAPI;
 
-  constructor(landingAPI: ILandingAPI) {
-    this.landingAPI = landingAPI;
-  }
+//   constructor(landingAPI: ILandingAPI) {
+//     this.landingAPI = landingAPI;
+//   }
 
-  async getSection(sectionType: SectionType): Promise<ISection> {
-    const response = await this.landingAPI.fetchSection(sectionType);
-    return response;
-  }
-}
+//   async getSection(sectionType: SectionType): Promise<ISection> {
+//     const response = await this.landingAPI.fetchSection(sectionType);
+//     return response;
+//   }
+// }
 
-// Apply decorators programmatically
-decorate(injectable(), SectionRepository);
+// // Apply decorators programmatically
+// decorate(injectable(), SectionRepository);
 
-// Create a factory function
-export const createSectionRepository = (landingAPI: ILandingAPI): ISectionRepository => {
-  return new SectionRepository(landingAPI);
-};
+// // Create a factory function
+// export const createSectionRepository = (landingAPI: ILandingAPI): ISectionRepository => {
+//   return new SectionRepository(landingAPI);
+// };
